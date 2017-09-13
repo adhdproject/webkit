6880de810f0dddac6447a73d17d55902d73636ee
zaeyx <ben@prometheaninfosec.com>
2017-09-13 14:40:09 +0000

    demo.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <a href="http://<?php echo $_SERVER['SERVER_ADDR']; ?>:3000/ui/panel">BeEF (Console)</a><br>
 <a href="/beef/hook.php" >BeEf (Hook)</a><br>
 <a href="/labyrinth/index.php">Weblabyrinth</a><br>
-<a href="/honeybadger/demo.html">Honey Badger (Location Tracker)</a><br>
+<a href="/honeybadger/demo.php">Honey Badger (Location Tracker)</a><br>
 <a href="/honeybadger/badger.php">Honey Badger (Reporting)</a><br>
 <a href="/web-bug-server/index.php">Web Bug Server</a><br>
 <a href="/decloak/index.php">Decloak</a><br>
